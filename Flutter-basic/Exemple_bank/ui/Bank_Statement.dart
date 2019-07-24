b31442ed30a3f39cf5ea8699346fa98f53d2de53
Andoly <andoly.cs@hotmail.com>
2019-07-24 00:09:46 -0300

    Saldo interativo

--- Flutter-basic/Exemple_bank/ui/Bank_Statement.dart
+++ Flutter-basic/Exemple_bank/ui/Bank_Statement.dart
@@ -1,52 +1,78 @@
-
 import 'package:flutter/material.dart';
 
 class Bank_Statement extends StatefulWidget {
   @override
   _Bank_StatementState createState() => _Bank_StatementState();
 }
-
 class _Bank_StatementState extends State<Bank_Statement> {
+  int _contadorGrana = 500;
+
+
+  // @ uso de underscore faz com que a função só possa ser chamada apenas nesse bloco.
+  void _emprestimo() {
+    //Responsável por atualizar os valores de exibição
+    setState((){
+      _contadorGrana = _contadorGrana + 100;
+    });
+  }
+  void _saldo() {
+    setState(() {
+      _contadorGrana = _contadorGrana - 900;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Extrato"),
-
       ),
       body: new Container(
         child: new Column(
           children: <Widget>[
             Center(
-              child: Text("Saldo atual ",
-              style: TextStyle(
-                fontSize: 15.6,
-                color: Colors.grey,
-                fontWeight: FontWeight.w700,
-              )
+                child: Text("Saldo atual ",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w700,
+                    )
+                )
+            ),
+
+            Expanded(child: Center(
+                child: Text("\$ $_contadorGrana",
+                  style: TextStyle(
+                      fontSize: 25.6,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700
+                  ),)
+            )),
+
+            Expanded(child: Center(
+                child: FlatButton(onPressed: _emprestimo ,
+                  color: Colors.lightGreen,
+                  textColor: Colors.white70,
+                  child: Text("Emprestimo de \$ 100 ",
+                      style: TextStyle(
+                        fontSize: 19.9,
+                        color: _contadorGrana >= 3000 ? Colors.blueAccent : Colors.white,
+                      )
+                  ),)
+            )),
+
+            Expanded(child: Center(
+              child: FlatButton(onPressed: _saldo,
+                  color: Colors.lightGreen,
+                  textColor: Colors.white,
+                  child: Text("Quitar divida de \$ 900 ",
+                    style: TextStyle(
+                      fontSize: 19.9,
+                      color: _contadorGrana < 0 ? Colors.red : Colors.white,
+                        )
+                    ),
             )
-          ),
-
-            Expanded(child: Center(
-              child: Text("\$ 1000",
-                style: TextStyle(
-                  fontSize: 25.6,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w700
-                ),
-              )
-            ),),
-
-            Expanded(child: Center(
-              child: FlatButton(onPressed: ()=> debugPrint("Controle financeiro"),
-              color: Colors.lightGreen,
-                textColor: Colors.white70,
-                child: Text("Extrato detalhado ",
-                style: TextStyle(
-                  fontSize: 19.9,
-                ))
-              )
-             ))
+            )),
 
           ],
         ),
